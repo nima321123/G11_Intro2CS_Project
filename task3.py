31de0940e27b7e0fa93484746a557af0d5cf5541
Mai Thi Yen Nhi <135790309+nima321123@users.noreply.github.com>
2023-10-29 23:32:06 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,160 +1,124 @@
-class Task3:
-    import torch
-    import torch.nn as nn
-    import torch.nn.functional as F
-    import torchvision
-    import torchvision.transforms as transforms
-    import matplotlib.pyplot as plt
-    import numpy as np
-    
+# Import the module
+import torchvision
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch
+import torch.optim as optim
+import torchmetrics
 
+# Download resnet18
+model = torchvision.models.resnet18(pretrained=True)
 
-    #device configuration
-    device = torch.device('cpu')
-    #hyper-parameters
-    num_epochs = 5
-    batch_size = 32
-    learning_rate = 0.001
+# Define transformation and Data augmentation for Train data
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(45),
+    transforms.ToTensor(),
+    transforms.Resize((224, 224)),
+])
+dataset_train = ImageFolder(
+    "C:\\Users\\Admin\\Desktop\\G11 IntroCS Project\\ProjectTemplate1\\dataset\\flowers",
+    transform = train_transform,
+)
 
-    
-    #define the transform to normalize and convert to a tensor
-    transform = transforms.Compose(
-        [  
-         transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    
-    #load the dataset
-    train_dataset = torchvision.datasets.ImageFolder(root='C:\\Users\Admin\\Desktop\\G11 IntroCS Project\\ProjectTemplate1\\dataset\\resize', transform=transform)
-    #create a data loader
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
-                                               shuffle=True)
-    
-    classes = ('daisy', 'dandelion', 'rose', 'sunflower', 'tulip')
+# Data augmentation for test data
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((224, 224)),
+])
+dataset_test = ImageFolder(
+    "C:\\Users\\Admin\Desktop\\G11 IntroCS Project\\ProjectTemplate1\\dataset\\smaller dataset\\flowers_test",
+    transform = test_transform
+)   
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+# Use Pytorch's DataLoader function to create a data loader
+dataloader_train = DataLoader(
+    dataset_train,
+    shuffle=True,
+    batch_size=32,
+)
 
-    
-    #function to display images
-    def imshow(img):
-        import matplotlib.pyplot as plt
-        import numpy as np
-        img = img / 2 + 0.5 #unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show()
-        plt.close()
+dataloader_test = DataLoader(
+    dataset_test,
+    shuffle=True,
+    batch_size=32,
+)
 
-    #get some random training images
-    dataiter = iter(train_loader)
-    images, labels = next(dataiter)
+# Freeze all the layers bar the last one
+for param in model.parameters():
+    param.requires_grad = False
 
-    
-    # Stack the resized images
-    stacked_images = torch.stack([img for img in images])
+# Change the number of output units
+# Classifier: more than one linear layer
+model.fc = nn.Sequential(
+   nn.Linear(512, 1024), # First linear layer
+   nn.ReLU(),           # Activation function
+   nn.Linear(1024, 512), # Second linear layer
+   nn.ReLU(),           # Activation function
+   nn.Linear(512, 5)    # Output layer with 5 units (for 5 flower species)
+)
 
-    #show images
-    imshow(torchvision.utils.make_grid(stacked_images))
+# Define a loss function and an optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    conv1 = nn.Conv2d(3, 6, 5)
-    pool = nn.MaxPool2d(2, 2)
-    conv2 = nn.Conv2d(6, 16, 5)
-    print(images.shape)
+# Train the network
+for epoch in range(5):  # loop over the dataset multiple times
 
-    
+    running_loss = 0.0
+    for i, data in enumerate(dataloader_train, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
 
-    input_size = 64  # Adjust this to match your input image size
-    conv1 = nn.Conv2d(3, 6, 5)
-    pool1 = nn.MaxPool2d(2, 2)
-    conv2 = nn.Conv2d(6, 16, 5)
-    pool2 = nn.MaxPool2d(2, 2)
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-    #implement conv net
-    class ConvNet(nn.Module):
+        # print statistics
+        running_loss += loss.item()
         
-        def __init__(self):
-            import torch
-            import torch.nn as nn
-            import torch.nn.functional as F
-            super().__init__()
+    print(f'Epoch {epoch + 1}, Loss: {running_loss / len(dataloader_train)}')
 
-            self.conv1 = nn.Conv2d(3, 6, 5)
-            self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 57 * 77, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 5)  # Adjust the output size according to your problem
+print('Finished Training')
 
-        def forward(self, x):
-            import torch
-            import torch.nn as nn
-            import torch.nn.functional as F
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
+# Calculate precision for each class after training
+class_correct = list(0. for i in range(5))
+class_total = list(0. for i in range(5))
+with torch.no_grad():
+    for i, data in enumerate(dataloader_test):
+        images, labels = data
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
 
-            # Flatten the feature map before passing it to the fully connected layer
-            x = x.view(-1, 16 * 57 *77)
-            
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = self.fc3(x)
-            return x
-    
-    
-    # Create an instance of the model
-    model = ConvNet()
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-    #total number of steps
-    n_total_steps = len(train_loader)
-
-    
-    # Training loop
-    for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(train_loader):
-            # Forward pass
-            
-            output = model(images)
-            loss = criterion(output, labels)
+        # Update the class-wise total count inside of the for loop.
+        class_total[labels.item()] += 1
 
 
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
 
-            if (i+1) % 2000 == 0:
-                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item()}')
+# Calculate precision for each class after training
+class_correct = list(0. for i in range(5))
+class_total = list(0. for i in range(5))
+with torch.no_grad():
+    for data in dataloader_test:
+        images, labels = data
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        label = labels.item()
+        class_correct[label] += c.item()
+        class_total[label] += 1
+classes = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
-    # Evaluation (optional)
-    with torch.no_grad():
-        # Calculate accuracy, class-specific accuracy, and overall accuracy
-        n_correct = 0
-        n_samples = 0
-        n_class_correct = [0 for i in range(5)]
-        n_class_samples = [0 for i in range(5)]
-        
-        for images, labels in train_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            
-            _, predicted = torch.max(outputs, 1)
-            n_samples += labels.size(0)
-            n_correct += (predicted == labels).sum().item()
+for i in range(5):
+    print('Precision of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
 
-            for i in range(5):
-                label = labels[i]
-                pred = predicted[i]
-                if (label == pred):
-                    n_class_correct[label] += 1
-                n_class_samples[label] += 1
 
-        for i in range(5):
-            acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-            print(f'Accuracy of {classes[i]}: {acc}%')
-
-        overall_acc = 100.0 * n_correct / n_samples
-        print(f'Overall Accuracy: {overall_acc:.2f}%')
