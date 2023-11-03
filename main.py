a974f2a7ddb27ddbe92495c4de7c2d17319c991a
Mai Thi Yen Nhi <135790309+nima321123@users.noreply.github.com>
2023-11-03 13:35:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from task3 import *
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
+task1 = Task1()
 task2 = Task2()
 task3 = Task3()
 
-scheduler.SCH_Add_Task(task2.Task2_Run, 3000,5000)
-scheduler.SCH_Add_Task(task3.Task3_Run, 8000,5000)
+scheduler.SCH_Add_Task(task1.Task1_Run, 1000,5000)
+scheduler.SCH_Add_Task(task2.Task2_Run, 1000,5000)
+scheduler.SCH_Add_Task(task3.Task3_Run, 1000,5000)
 
 while True:
     scheduler.SCH_Update()
