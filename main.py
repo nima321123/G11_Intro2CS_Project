14b43e437ddd378ddfb4819addea580bf5774107
Mai Thi Yen Nhi <135790309+nima321123@users.noreply.github.com>
2023-11-14 00:20:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from task3 import *
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
-task3 = Task3()
-task2 = Task2()
 task1 = Task1()
+task2 = Task2()
+task3 = Task3()
 
-scheduler.SCH_Add_Task(task3.update_result_text, 40000 ,0)
-scheduler.SCH_Add_Task(task2.analyze_weather("d:\Downloads\VGU\Intro to CS\RTOS\BerlinGermany.csv"), 20000 ,0)
+
 scheduler.SCH_Add_Task(task1.Task1_Run, 0,0)
+scheduler.SCH_Add_Task(task2.analyze_weather("d:\Downloads\VGU\Intro to CS\RTOS\BerlinGermany.csv"), 20000 ,0)
+scheduler.SCH_Add_Task(task3.update_result_text, 40000 ,0)
 
 while True:
     scheduler.SCH_Update()
