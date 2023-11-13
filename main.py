0926cc85204bcb169295e125dbac0121e76b3b19
hejmanh <135790228+hejmanh@users.noreply.github.com>
2023-11-13 16:59:30 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from task1 import *
 from task2 import *
 from task3 import *
 
+
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
-task1 = Task1()
-task2 = Task2()
 task3 = Task3()
+task2 = Task2()
+task1 = Task1()
 
-scheduler.SCH_Add_Task(task1.Task1_Run, 1000,5000)
-scheduler.SCH_Add_Task(task2.Task2_Run, 1000,5000)
-scheduler.SCH_Add_Task(task3.Task3_Run, 1000,5000)
+scheduler.SCH_Add_Task(task3.update_result_text, 40000 ,0)
+scheduler.SCH_Add_Task(task2.analyze_weather("d:\Downloads\VGU\Intro to CS\RTOS\BerlinGermany.csv"), 20000 ,0)
+scheduler.SCH_Add_Task(task1.Task1_Run, 0,0)
 
 while True:
     scheduler.SCH_Update()
