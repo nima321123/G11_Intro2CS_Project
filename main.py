05744e44be29c8a8ce00e19e4f6190071daa4fed
hejmanh <135790228+hejmanh@users.noreply.github.com>
2023-11-14 00:43:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ task2 = Task2()
 task3 = Task3()
 
 
-scheduler.SCH_Add_Task(task1.Task1_Run, 0,0)
+scheduler.SCH_Add_Task(task1.update_result_text, 0,0)
 scheduler.SCH_Add_Task(task2.analyze_weather("d:\Downloads\VGU\Intro to CS\RTOS\BerlinGermany.csv"), 20000 ,0)
-scheduler.SCH_Add_Task(task3.update_result_text, 40000 ,0)
+scheduler.SCH_Add_Task(task3.Task1_Run, 40000)
 
 while True:
     scheduler.SCH_Update()
