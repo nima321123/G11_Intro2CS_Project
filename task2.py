a333d8f5813f4535cd95ab79457d5e75379a51b3
ThaoVy <95116467+Arroxkun@users.noreply.github.com>
2023-11-13 19:00:46 +0700

    task2.py

--- task2.py
+++ task2.py
@@ -1,127 +1,115 @@
-
 class Task2:
+    def analyze_weather(self, file_path):
+        # Load data
+        weather = pd.read_csv(file_path, index_col="datetime")
 
-    import pandas as pd
-    weather = pd.read_csv("/content/drive/MyDrive/Weather_dataset/BerlinGermany.csv", index_col="datetime")
+        # Display unique values in each column
+        for col in weather.columns:
+            unique_values = weather[col].unique()
+            print(f"Column {col}: {unique_values}")
 
-    weather
+        # Handle non-numeric values
+        weather.replace("your_non_numeric_value", pd.NA, inplace=True)
+        weather = weather.apply(pd.to_numeric, errors='coerce')
+        weather = weather.ffill().fillna(0)
 
-    null_pct = weather.apply(pd.isnull).sum()/weather.shape[0]
-    null_pct
+        # Continue with your existing logic
+        null_pct = weather.apply(pd.isnull).sum() / weather.shape[0]
+        valid_columns = weather.columns[null_pct < 0.03]
 
-    valid_columns = weather.columns[null_pct < .03]
+        print("Valid Columns:")
+        print(valid_columns)
 
-    valid_columns
+        # Count occurrences of 9999 in each column
+        occurrences_9999 = weather.apply(lambda x: (x == 9999).sum())
+        print("Occurrences of 9999 in each column:")
+        print(occurrences_9999)
 
-    weather.apply(lambda x: (x == 9999).sum())
+        # Data types of each column
+        data_types = weather.dtypes
+        print("Data Types:")
+        print(data_types)
 
-    weather.dtypes
+        # Display the index
+        index_values = weather.index
+        print("Index Values:")
+        print(index_values)
 
-    weather.index
+        # Convert the index to datetime
+        weather.index = pd.to_datetime(weather.index)
 
-    weather.index = pd.to_datetime(weather.index)
+        # Display the count of years in the index
+        year_counts = weather.index.year.value_counts().sort_index()
+        print("Year Counts:")
+        print(year_counts)
 
-    weather.index.year.value_counts().sort_index( )
+        # Plot the 'tempmax' column
+        weather["tempmax"].plot(title="Temperature Max")
 
-    weather ["tempmax"].plot()
+        # Create the 'target' column by shifting 'tempmax'
+        weather["target"] = weather["tempmax"].shift(-1)
 
-    weather["target"] = weather.shift(-1)["tempmax"]
+        # Display the modified DataFrame
+        print("Modified DataFrame:")
+        print(weather)
 
-    weather
+        # Forward fill missing values
+        weather = weather.ffill()
+        print("DataFrame after Forward Fill:")
+        print(weather)
 
-    weather = weather.ffill()
-    weather
+        # Calculate correlation matrix
+        correlation_matrix = weather.corr()
+        print("Correlation Matrix:")
+        print(correlation_matrix)
+        rr = Ridge(alpha=0.1)
 
-    weather.corr()
+        predictors = weather.columns[~weather.columns.isin(["target", "name", "station", "preciptype", "sunrise", "sunset", "conditions", "description", "icon", "stations"])]
 
-    from sklearn.linear_model import Ridge
-    rr = Ridge(alpha=.1)
+        def backtest(weather, model, predictors, start=30, step=10):
+            all_predictions = []
 
-    predictors = weather.columns[~weather.columns.isin(["target", "name", "station","preciptype","sunrise","sunset","conditions", "description", "icon","stations"])]
+            for i in range(start, weather.shape[0], step):
+                train = weather.iloc[:i, :]
+                test = weather.iloc[i:(i+step), :]
 
-    predictors
+                model.fit(train[predictors], train["target"])
 
-    def backtest(weather, model, predictors, start=30, step=10):
-        all_predictions = []
+                preds = model.predict(test[predictors])
+                preds = pd.Series(preds, index=test.index)
+                combined = pd.concat([test["target"], preds], axis=1)
+                combined.columns = ["actual", "prediction"]
+                combined["diff"] = (combined["prediction"] - combined["actual"]).abs()
 
-        for i in range(start, weather.shape[0], step):
-            train = weather.iloc[:i,:]
-            test = weather.iloc[i:(i+step),:]
+                all_predictions.append(combined)
+            return pd.concat(all_predictions)
 
-            model.fit(train[predictors], train["target"])
+        weather = weather.fillna(0)
 
-            preds = model.predict(test[predictors])
-            preds = pd.Series(preds, index=test.index)
-            combined = pd.concat([test["target"], preds], axis=1)
-            combined.columns = ["actual", "prediction"]
-            combined["diff"] = (combined["prediction"] - combined["actual"]).abs()
+        predictions = backtest(weather, rr, predictors)
+      # Print or return any relevant results
+        print("Predictions:")
+        print(predictions)
 
-            all_predictions.append(combined)
-        return pd.concat(all_predictions)
+        # Evaluate model performance
+        mae = mean_absolute_error(predictions["actual"], predictions["prediction"])
+        mse = mean_squared_error(predictions["actual"], predictions["prediction"])
+        print("Mean Absolute Error:", mae)
+        print("Mean Squared Error:", mse)
 
-    weather= weather.fillna(0)
+        # Display additional results as needed
+        print("Top 5 predictions with the largest differences:")
+        print(predictions.sort_values("diff", ascending=False).head())
 
-    predictions = backtest(weather, rr, predictors)
+        # Subset of data for May 2021
+        print("Subset of data for May 2021:")
+        print(weather.loc["2021-05-01": "2021-05-10"])
 
-    predictions
+        # Distribution of rounded differences in predictions
+        print("Distribution of rounded differences in predictions:")
+        print(predictions["diff"].round().value_counts().sort_index())
 
-    from sklearn.metrics import mean_absolute_error, mean_squared_error
-
-    mean_absolute_error(predictions["actual"], predictions["prediction"])
-
-    predictions["diff"].mean()
-
-    pd.Series(rr.coef_, index=predictors)
-
-    def pct_diff(old, new):
-        return (new - old) / old
-
-    def compute_rolling(weather, horizon, col):
-        label = f"rolling_{horizon}_{col}"
-        weather[label] = weather[col].rolling(horizon).mean()
-        weather[f"{label}_pct"] = pct_diff(weather[label], weather[col])
-        return weather
-
-    rolling_horizons = [3, 10]
-    for horizon in rolling_horizons:
-        for col in ["tempmax", "tempmin", "precip"]:
-            weather = compute_rolling(weather, horizon, col)
-
-    weather
-
-    weather = weather.iloc[10:,:]
-
-    weather
-
-    weather = weather.fillna(0)
-
-    def calculate_expanding_mean(column):
-        return column.expanding(1).mean()
-
-    columns_to_process = ["tempmax", "tempmin", "precip"]
-
-    for col in columns_to_process:
-        month_avg_col_name = f"month_avg_{col}"
-        day_avg_col_name = f"day_avg_{col}"
-
-        weather[month_avg_col_name] = weather.groupby(weather.index.month)[col].transform(calculate_expanding_mean)
-        weather[day_avg_col_name] = weather.groupby(weather.index.dayofyear)[col].transform(calculate_expanding_mean)
-
-    weather
-
-    predictors = weather.columns[~weather.columns.isin(["target", "name", "station","preciptype","sunrise","sunset","conditions", "description", "icon","stations"])]
-
-    predictors
-
-    predictions = backtest(weather, rr, predictors)
-    mean_absolute_error(predictions["actual"], predictions["prediction"])
-
-    mean_squared_error(predictions["actual"], predictions["prediction"])
-
-    predictions.sort_values("diff", ascending=False)
-
-    weather.loc["2021-05-01": "2021-05-10"]
-
-    predictions["diff"].round().value_counts().sort_index()
-
-    predictions
+if __name__ == "__main__":
+    file_path = "E:/ProjectTemplate1/BerlinGermany.csv"
+    task2_instance = Task2()
+    task2_instance.analyze_weather(file_path)
